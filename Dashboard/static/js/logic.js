2ce7b2e95f2f4927ac95dcec9192107261b708f8
Hunter Carlisle <huntingcarlisle@gmail.com>
2021-06-10 15:48:29 -0700

    setup connection to database for csv data

--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -15,20 +15,8 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
               accessToken: API_KEY})
             .addTo(myMap);
 
-
-// Path to data
-var dataPath = "../static/data/sample_set.csv";
-
 //Clear layer group
 var markers = L.layerGroup().addTo(myMap)
-
-// Grab data with d3
-  // d3.csv(dataPath).then(function(data) {
-  // // Creating a GeoJSON layer with the retrieved data
-  //   L.geoJson(data)
-  //    .addTo(myMap);
-  // });
-
 
 // JavaScript here - event listeners and event handlers
 // Select the button
@@ -52,31 +40,28 @@ function runFind() {
   var inputCounty = d3.select("#example-form-input1").property("value");
   var inputHouseType = d3.select("#example-form-input2").property("value");
 
-  // Getting all data in
-  d3.csv(dataPath).then(function(datain) {
-    // Filtering data
-    var filteredCounty = datain.filter(rowItem => rowItem.county === inputCounty)
-                               .filter(rowItem => rowItem.house_type === inputHouseType)
+  // Filtering data
+  var filteredCounty = data.filter(rowItem => rowItem.county === inputCounty)
+                           .filter(rowItem => rowItem.house_type === inputHouseType)
+  
+  // testData = filteredCounty
+
+  console.log(filteredCounty)
+  console.log(filteredCounty.map(rowItem => rowItem.lat))
+
+  filteredCounty.forEach(function(rowItem) {
+    markers.addLayer(L.marker([rowItem.lat, rowItem.lng])
+                    .bindPopup("<h3>" + rowItem.zipcode + "</h3> <hr> <h4>Estimated Home Price: " + rowItem.est_price + "</h4>"))
+  })
+
     
-    // testData = filteredCounty
-
-    console.log(filteredCounty)
-    console.log(filteredCounty.map(rowItem => rowItem.lat))
-
-    filteredCounty.forEach(function(rowItem) {
-      markers.addLayer(L.marker([rowItem.lat, rowItem.lng])
-                      .bindPopup("<h3>" + rowItem.zipcode + "</h3> <hr> <h4>Estimated Home Price: " + rowItem.est_price + "</h4>"))
-    })
-
-      
-      var southWest = L.latLng(math.min(filteredCounty.map(rowItem => rowItem.lat)), 
-                               math.min(filteredCounty.map(rowItem => rowItem.lng)))
-      northEast = L.latLng(math.max(filteredCounty.map(rowItem => rowItem.lat)), 
-      math.max(filteredCounty.map(rowItem => rowItem.lng)))
-      bounds = L.latLngBounds(southWest, northEast);
+    var southWest = L.latLng(math.min(filteredCounty.map(rowItem => rowItem.lat)), 
+                              math.min(filteredCounty.map(rowItem => rowItem.lng)))
+    northEast = L.latLng(math.max(filteredCounty.map(rowItem => rowItem.lat)), 
+    math.max(filteredCounty.map(rowItem => rowItem.lng)))
+    bounds = L.latLngBounds(southWest, northEast);
       
   myMap.flyToBounds(bounds);
-  });   
 }
 
   // Create event handlers for clicking the button 
