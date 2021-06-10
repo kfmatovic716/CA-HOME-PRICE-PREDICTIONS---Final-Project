3d04398b1bdc1473dba42bc2ebab98bd14d7c509
Katherine Matovic <jabuka611@gmail.com>
2021-06-09 22:30:06 -0700

    Add schema and update js & readme file

--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -1,7 +1,7 @@
 // Create initial map object centered in Los Angeles
 var myMap = L.map("map", 
                   {center: [34.05, -118.24],
-                   zoom: 13
+                   zoom: 15
                 });
 
 // Add the background map image (tile layer)
@@ -15,6 +15,54 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
               accessToken: API_KEY})
             .addTo(myMap);
 
+// TO DO! Used arbitrary filename because don't know how the flask output will be
+//link to get the geojson data
+var geojsonPath = "static/data/homeprice.geojson";
+
+// Grab data with d3
+d3.json(geojsonPath).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data)
+   .addTo(myMap);
+});
+
+//console.log(data)
+
+
+// JavaScript here - event listeners and event handlers
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create the function to run for both events
+function runFind() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element of county name (inputElement1) and house type (inputElement2)
+  // Get the value property of the input element
+  var inputCounty = d3.select("#example-form-input").property("value");
+  // var inputHouseType = d3.select("#example-form-input").property("value");
+
+  // Print the value to the console
+  // console.log(inputValue);
+
+  var filteredCounty = 
+  // var filteredHouseType =
+
+  filteredCounty.
+
+
+  
+  // Set the span tag in the h1 element to the text
+  // that was entered in the form
+  d3.select("h1>span").text(inputValue);
+}
+
+filterButton.on('click', runFind);
 // Each counties object contains the different zip codes and predicted price
 var counties = [
                 {
@@ -52,3 +100,12 @@ counties.forEach(city => {
       
     }).bindPopup("<h3>" + city.zipcode + "</h3> <hr> <h4>Estimated Home Price: " + city.estimated_price + "</h4>").addTo(myMap);
   })
+
+
+
+
+
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runFind);
+form.on("submit", runFind);
