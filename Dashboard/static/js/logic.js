0a36d069b4479dcfc290752be2ac67223a229ed9
Katherine Matovic <jabuka611@gmail.com>
2021-06-17 22:44:33 -0700

    Update dashboard files and add js code for dropdwn

--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -25,6 +25,23 @@ var button = d3.select("#button");
 // Select the form
 var form = d3.select("#form");
 
+// Appends the dropdown items on county and housetype 
+// Allows the user to select from these items
+function appendDrpdwn(items) {
+  // iterates county names
+  var countyItems = Object.values(items).forEach(county => console.log(county));
+    // appends county names to the dropdown
+    countyItems.forEach(county => {
+      selDropdwnCounty.append("option").text(county).property("value", county)
+    });
+
+  // iterates available house types
+  var housetypeItems = Object.values(items).forEach(house_type => console.log(house_type));
+    // appends house types to the dropdown
+    housetypeItems.forEach(house_type => {
+      selDropdwnType.append("option").text(house_type).property("value", house_type)
+    });
+}
 
 // Create the function to run for both events
 function runFind() {
@@ -32,15 +49,23 @@ function runFind() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
+  // Populates dropdown menu 
+  var selDropdwnCounty = d3.select("#selCounty");
+  var selDropdwnType = d3.select("#selHouseType");
+
+  // calls the appendDrpdwn function to append items from the data
+  data.forEach(appendDrpdwn);
+
+
   // Removes markers
  markers.clearLayers()
 
   // Select the input element of county name (inputElement1) and house type (inputElement2)
   // Get the value property of the input element
-  var inputCounty = d3.select("#example-form-input1").property("value");
-  var inputHouseType = d3.select("#example-form-input2").property("value");
+  var inputCounty = d3.select("#selCounty").property("value");
+  var inputHouseType = d3.select("#selHouseType").property("value");
 
-  // Filtering data. Used trim() because county names have additional space after the neme. Can be fixed later 
+  // Filtering data. Used trim() because county names have additional space after the name. Can be fixed later 
   var filteredCounty = data.filter(rowItem => rowItem.county.trim() == inputCounty)
                             .filter(rowItem => rowItem.house_type === inputHouseType)
   
@@ -48,24 +73,22 @@ function runFind() {
   console.log(filteredCounty)
   console.log(filteredCounty.map(rowItem => rowItem.lat))
 
+
+// Prints out the selected zipcode and estimated price in the tooltip
   filteredCounty.forEach(function(rowItem) {
     markers.addLayer(L.marker([rowItem.lat, rowItem.lng])
                     .bindPopup("<h4>Zip Code: " + rowItem.zipcode + "</h4> <hr> <h5>Estimated Home Price: $ " + rowItem.est_price + "</h5>"))
   })
 
-    
+ // Calculates the location bounds based on user input selection   
     var southWest = L.latLng(math.min(filteredCounty.map(rowItem => rowItem.lat)), 
                               math.min(filteredCounty.map(rowItem => rowItem.lng)))
     northEast = L.latLng(math.max(filteredCounty.map(rowItem => rowItem.lat)), 
     math.max(filteredCounty.map(rowItem => rowItem.lng)))
     bounds = L.latLngBounds(southWest, northEast);
-      
+    
+ // map flies and zooms in to the filtered location
   myMap.flyToBounds(bounds);
 }
-
-  // Create event handlers for clicking the button 
-button.on('click', runFind);
-
-
 
 
